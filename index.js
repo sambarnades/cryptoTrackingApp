8aadc2bef82cefd03bbf4ed449bbdccaa0028194
Sam Barnades <77226711+sambarnades@users.noreply.github.com>
2023-09-13 23:00:22 +0200

    Succeed to list all the cryptos with the icon (except for no referenced.) Add a bit of CSS.

--- index.js
+++ index.js
@@ -11,16 +11,26 @@ app.use(express.static(`public`));
 app.get(`/`, async (req, res) => {
     try {
         const response = await axios.get(`https://api.blockchain.com/v3/exchange/tickers`);
-        
-        amountOfCryptocurrencies = response.data.length
-        amountOfRows = Math.floor(amountOfCryptocurrencies / 4) + 1
-        
-        console.log(response.data[0]);  
 
-        res.render(`index.ejs`, {
-            data: response.data,
+        let data = response.data;                                               // Declare all useful variables
+        let amountOfCryptocurrencies = data.length;
+        let amountOfRows = Math.floor(amountOfCryptocurrencies / 4) + 1;
+        let cryptoNames = [];
+
+        data.forEach(cryptoCurrency => {                                    // Loop for all the data array, for each crypto currency.
+
+            let cryptoConversion = cryptoCurrency.symbol;                   // Transform the conversion title to the crypto name only. Useful to link to the svg file for the icon.
+            let hyphenIndex = cryptoConversion.indexOf('-');
+            let cryptoName = cryptoConversion.substring(0, hyphenIndex).toLowerCase();
+
+            cryptoNames.push(cryptoName);       // Put the name only into an array.
+        });
+
+        res.render(`index.ejs`, {  
+            data: data,
             amountOfCryptocurrencies: amountOfCryptocurrencies,
-            amountOfRows: amountOfRows
+            amountOfRows: amountOfRows,
+            cryptoNames: cryptoNames
         });
 
     } catch (error) {
