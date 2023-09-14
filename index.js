76af70be501894a67cce99b969cc457b1c456a10
Sam Barnades <77226711+sambarnades@users.noreply.github.com>
2023-09-15 00:21:31 +0200

    Try to put the right currency for each crypto. Doesn't work at the moment.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require(`axios`).default;
 const jquery = require(`jquery`);
 
 const app = express();
-const port = 4000;
+const port = 4001;
 
 app.use(express.static(`public`));
 
@@ -16,12 +16,29 @@ app.get(`/`, async (req, res) => {
         let amountOfCryptocurrencies = data.length;
         let amountOfRows = Math.floor(amountOfCryptocurrencies / 4) + 1;
         let cryptoNames = [];
+        let fiatCurrency = [];
 
         data.forEach(cryptoCurrency => {                                    // Loop for all the data array, for each crypto currency.
 
             let cryptoConversion = cryptoCurrency.symbol;                   // Transform the conversion title to the crypto name only. Useful to link to the svg file for the icon.
             let hyphenIndex = cryptoConversion.indexOf('-');
             let cryptoName = cryptoConversion.substring(0, hyphenIndex).toLowerCase();
+
+            fiatCurrency.push(cryptoConversion.substring(hyphenIndex + 1, cryptoConversion.length).toUpperCase()); // Extract the FIAT conversion currency. Doesn't work.
+
+            console.log(fiatCurrency[cryptoCurrency]);
+
+
+                if(fiatCurrency[cryptoCurrency] == "EUR") {
+                    
+                    fiatCurrency[cryptoCurrency] = "€";
+                }
+                  
+                else if(fiatCurrency[cryptoCurrency] <= "USD") {
+                    fiatCurrency[cryptoCurrency] = "$"
+                }
+                
+
 
             cryptoNames.push(cryptoName);       // Put the name only into an array.
         });
@@ -30,7 +47,8 @@ app.get(`/`, async (req, res) => {
             data: data,
             amountOfCryptocurrencies: amountOfCryptocurrencies,
             amountOfRows: amountOfRows,
-            cryptoNames: cryptoNames
+            cryptoNames: cryptoNames,
+            fiatCurrency: fiatCurrency
         });
 
     } catch (error) {
